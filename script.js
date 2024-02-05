597590aa83b9a7d47bb655b68fd44feec393a507
Udhav Jadhav <72942660+udhavjadhav@users.noreply.github.com>
2024-02-05 22:12:59 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,9 @@
+// Add this JavaScript code in your script.js file
+document.getElementById('google-signup').addEventListener('click', function() {
+    // You need to implement Google Signup functionality here.
+    alert('Google Signup clicked');
+ });
+ 
+function submit(){
+    alert("Registration successful!")
+}
